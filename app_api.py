83c902c2eb7a37e9e579176fe5eb07fb6ebc06bf
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-04-02 16:01:46 +0200

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -15,7 +15,7 @@ ma = Marshmallow(app)
 
 class Computer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
+    computer_name = db.Column(db.String(100), unique=True)
     hard_drive = db.Column(db.VARCHAR(100))
     processor = db.Column(db.VARCHAR(100))
     ram_amount = db.Column(db.Float)
@@ -23,8 +23,8 @@ class Computer(db.Model):
     hard_drive_space = db.Column(db.VARCHAR(100))
     form_factor = db.Column(db.VARCHAR(100))
 
-    def __init__(self, name, hard_drive, processor, ram_amount, maximum_ram, hard_drive_space, form_factor):
-        self.name = name
+    def __init__(self, computer_name, hard_drive, processor, ram_amount, maximum_ram, hard_drive_space, form_factor):
+        self.computer_name = computer_name
         self.hard_drive = hard_drive
         self.processor = processor
         self.ram_amount = ram_amount
@@ -34,14 +34,14 @@ class Computer(db.Model):
 
 class ComputerSchema(ma.Schema):
     class Meta:
-        fields = ('id','name','hard_drive', 'processor', 'ram_amount', 'maximum_ram', 'hard_drive_space', 'form_factor')
+        fields = ('id','computer_name','hard_drive', 'processor', 'ram_amount', 'maximum_ram', 'hard_drive_space', 'form_factor')
 
 computer_schema = ComputerSchema(strict=True)
 computers_schema = ComputerSchema(many=True, strict=True)
 
-@app.route('/computer', methods=['POST'])
+@app.route('/computers', methods=['POST'])
 def add_computer():
-    name = request.json['name']
+    computer_name = request.json['computer_name']
     hard_drive = request.json['hard_drive']
     processor = request.json['processor']
     ram_amount = request.json['ram_amount']
@@ -50,29 +50,29 @@ def add_computer():
     form_factor = request.json['form_factor']
     
 
-    new_computer = Computer(name, hard_drive, processor, ram_amount, maximum_ram, hard_drive_space, form_factor)
+    new_computer = Computer(computer_name, hard_drive, processor, ram_amount, maximum_ram, hard_drive_space, form_factor)
     db.session.add(new_computer)
     db.session.commit()
 
     return computer_schema.jsonify(new_computer)
 
-@app.route('/computer', methods=['GET'])
+@app.route('/computers', methods=['GET'])
 def get_computers():
     all_computers = Computer.query.all()
     result = computers_schema.dump(all_computers)
     return jsonify(result.data)
 
-@app.route('/computer/<id>', methods=['GET'])
+@app.route('/computers/<id>', methods=['GET'])
 def get_computer(id):
     computer = Computer.query.get(id)
     # result = computers_schema.dump(all_computers)
     return computer_schema.jsonify(computer)
 
-@app.route('/computer/<id>', methods=['PUT'])
+@app.route('/computers/<id>', methods=['PUT'])
 def update_computer(id):
     computer = Computer.query.get(id)
 
-    name = request.json['name']
+    computer_name = request.json['computer_name']
     hard_drive = request.json['hard_drive']
     processor = request.json['processor']
     ram_amount = request.json['ram_amount']
@@ -80,7 +80,7 @@ def update_computer(id):
     hard_drive_space = request.json['hard_drive_space']
     form_factor = request.json['form_factor']
 
-    computer.name = name
+    computer.computer_name = computer_name
     computer.hard_drive = hard_drive
     computer.processor = processor
     computer.ram_amount = ram_amount
