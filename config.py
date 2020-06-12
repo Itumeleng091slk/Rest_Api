cb84e2c38ac35f6d38672c2a3fe1fd0a14711cf9
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-06-12 20:30:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@
 connect_to_database = psycopg2.connect(database="data_sql",user="user",password="pass", host="localhost",port=5432)
 os_path = os.path.abspath(os.path.dirname(__file__))
 connect_to_postgres = 'postgresql:///' + os.path.join(basedir, 'postgresql')
+connect_to_engine = 'postgresql+psycopg2://user:pass@localhost:5432/data_sql'
