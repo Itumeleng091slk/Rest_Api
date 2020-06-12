24e33f6f194106ebae8019b6f074b478230dd6ac
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-06-12 20:25:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-import os
+# import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_ECHO = False
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://user:pass@localhost/data_sql"
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# SQLALCHEMY_ECHO = False
+# SQLALCHEMY_TRACK_MODIFICATIONS = True
+# SQLALCHEMY_DATABASE_URI = "postgresql://user:pass@localhost/data_sql"
 
+connect_to_database = psycopg2.connect(database="data_sql",user="user",password="pass", host="localhost",port=5432)
