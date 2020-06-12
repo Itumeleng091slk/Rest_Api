bfe107a6c37c9422da65e8b0d6d1cea5a1b61aa1
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-06-12 20:26:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@
 # SQLALCHEMY_DATABASE_URI = "postgresql://user:pass@localhost/data_sql"
 
 connect_to_database = psycopg2.connect(database="data_sql",user="user",password="pass", host="localhost",port=5432)
+os.path.abspath(os.path.dirname(__file__))
